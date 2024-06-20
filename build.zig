36b0bfa13081b45c6e167586e1a4ecc07536a13d
Dumb Bird <72983221+ZackeryRSmith@users.noreply.github.com>
2024-06-20 12:45:50 -0400

    fix build.zig
    This commit hopefully should allow `zig fetch` to work properly

--- build.zig
+++ build.zig
@@ -4,19 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zinput",
+    _ = b.addModule("zinput", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(lib);
 
-    const lib_unit_tests = b.addTest(.{
+    const tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&lib_unit_tests.step);
+    const tests_step = b.step("test", "Run library tests");
+    tests_step.dependOn(&tests.step);
 }
