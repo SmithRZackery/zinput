1e3503fa74f139173c259d58905e97f289f105c4
ZackeryRSmith <zackery.smith82307@gmail.com>
2024-06-20 13:51:02 -0400

    attempt to auto include system headers

--- build.zig
+++ build.zig
@@ -4,11 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zinput", .{
+    const module = b.addModule("zinput", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    module.link_libc = true;
+
+    if (target.result.os.tag == .windows) {
+        module.addSystemIncludePath(b.path("windows.h"));
+    } else if (target.result.os.tag == .macos) {
+        module.addSystemIncludePath(b.path("CoreGraphics/CoreGraphics.h"));
+    } else {
+        // NOTE: I'll likely need to ship both X11 and Wayland in this project.
+        //       A Linux binary must have both compiled in for it to work.
+        //       I shall look into using Mach Core's X11 and Wayland mirrors
+
+        // module.addSystemIncludePath("wayland-client.h");
+        module.addSystemIncludePath(b.path("xdo.h"));
+    }
 
     const tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
